c430990ea515dd60e1d2250e425bcd70188dcb63
Misbah Aamer <misbahuddin.aamer@gmail.com>
2022-03-21 03:12:28 -0400

    test create user api with schema

--- usrmgt/src/handlers.ts
+++ usrmgt/src/handlers.ts
@@ -1,4 +1,58 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import AWS from "aws-sdk";
+import { v4 } from "uuid";
+import * as yup from "yup";
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+const tableName = "UsersTable";
+const headers = {
+  "content-type": "application/json",
+};
+
+const schema = yup.object().shape({
+  firstName: yup.string().required(),
+  middleInitial: yup.string().required(),
+  lastName: yup.string().required(),
+  gender: yup.string().required(),
+  email: yup.string().required(),
+  phoneNumber: yup.string().required(),
+});
+
+class HttpError extends Error {
+  constructor(public statusCode: number, body: Record<string, unknown> = {}) {
+    super(JSON.stringify(body));
+  }
+}
+
+const handleError = (e: unknown) => {
+  if (e instanceof yup.ValidationError) {
+    return {
+      statusCode: 400,
+      headers,
+      body: JSON.stringify({
+        errors: e.errors,
+      }),
+    };
+  }
+
+  if (e instanceof SyntaxError) {
+    return {
+      statusCode: 400,
+      headers,
+      body: JSON.stringify({ error: `invalid request body format : "${e.message}"` }),
+    };
+  }
+
+  if (e instanceof HttpError) {
+    return {
+      statusCode: e.statusCode,
+      headers,
+      body: e.message,
+    };
+  }
+
+  throw e;
+};
 
 export const hello = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   return {
@@ -12,4 +66,32 @@ export const hello = async (event: APIGatewayProxyEvent): Promise<APIGatewayProx
       2,
     ),
   };
+};
+
+export const createUser = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  try {
+    const reqBody = JSON.parse(event.body as string);
+
+    await schema.validate(reqBody, { abortEarly: false });
+
+    const product = {
+      ...reqBody,
+      productID: v4(),
+    };
+
+    await docClient
+      .put({
+        TableName: tableName,
+        Item: product,
+      })
+      .promise();
+
+    return {
+      statusCode: 201,
+      headers,
+      body: JSON.stringify(product),
+    };
+  } catch (e) {
+    return handleError(e);
+  }
 };
